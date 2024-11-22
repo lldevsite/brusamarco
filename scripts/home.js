29fa2735a237c0d64f5cc6652566e01d8f2623c6
coffeeculturedesign <lldevsite@gmail.com>
2024-11-22 18:31:50 +0100

    path update

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', ()=>{
     load('./components/navbar.html', 'navbar');
-    load('../components/footer.html', 'footer');
+    load('./components/footer.html', 'footer');
 
 });
