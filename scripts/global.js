47541466358dd4116e7336955c73952840b482ba
coffeeculturedesign <lldevsite@gmail.com>
2024-12-01 16:37:49 +0100

    mobile update

--- scripts/global.js
+++ scripts/global.js
@@ -8,7 +8,9 @@ window.addEventListener('load', () => {
         let src = oc.src.split('/');
 
         if (src[src.length - 1] == 'hamburger.svg') {
+            document.documentElement.style.overflowY = 'hidden';
             document.getElementsByClassName('navbar-links')[0].classList.remove('fadeout');
+            document.getElementsByClassName('navbar-links')[0].style.display = 'flex';
             void document.getElementsByClassName('navbar-links')[0].offsetWidth;
             document.getElementsByClassName('navbar-links')[0].classList.add('fadein');
             oc.src = './svg/cross.svg';
@@ -18,6 +20,11 @@ window.addEventListener('load', () => {
             void document.getElementsByClassName('navbar-links')[0].offsetWidth;
             document.getElementsByClassName('navbar-links')[0].classList.add('fadeout');
             oc.src = './svg/hamburger.svg';
+            document.documentElement.style.overflowY = 'scroll';
+            document.getElementsByClassName('navbar-links')[0].addEventListener('animationend', function handleCloseMenu () {
+                document.getElementsByClassName('navbar-links')[0].style.display = 'none';
+                document.getElementsByClassName('navbar-links')[0].removeEventListener('animationend', handleCloseMenu);
+            });
         }
 
 
